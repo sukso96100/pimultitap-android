8053974ac85fb06b973bd3f9ec4c7017f378cf6b
sukso96100 <sukso96100@gmail.com>
2015-07-19 19:47:20 +0900

    working on connection activity

--- app/src/main/java/com/youngbin/pimultitap/ConnectActivity.java
+++ app/src/main/java/com/youngbin/pimultitap/ConnectActivity.java
@@ -2,37 +2,39 @@ package com.youngbin.pimultitap;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class ConnectActivity extends ActionBarActivity {
+public class ConnectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+        getSupportActionBar().hide();
+
+        Button ConnectBtn = (Button)findViewById(R.id.connectbtn);
+        Button CancelBtn = (Button)findViewById(R.id.cancelbtn);
+        TextView StateTxt = (TextView)findViewById(R.id.state);
+
+        ConnectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        CancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_connect, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
